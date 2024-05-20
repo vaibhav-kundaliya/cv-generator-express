2fa04665ceddb9b7a1316d99d356c3446ba203dd
vaibhav-kundaliya <vaibhav.kundaliya@iviewlabs.net>
2024-05-20 18:23:03 +0530

    left-right margin 100px

--- server.js
+++ server.js
@@ -621,7 +621,7 @@ app.post("/", (req, res) => {
                         </div>
                     </div>
                     
-                    <div style="top: 20pt">
+                    <div style="top: 20pt; margin-left:100px; margin-right:100px">
                         ${content}
                     </div>
                 </body>

0acb535757d9591b3d81e07ec7de5265a3e57fbc
vaibhav-kundaliya <vaibhav.kundaliya@iviewlabs.net>
2024-05-22 13:42:08 +0530

    font changed

--- server.js
+++ server.js
@@ -576,9 +576,11 @@ app.post("/", (req, res) => {
     <!DOCTYPE html>
             <html>
                 <head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
                     <title>${name} white labeled cv</title>
                     <style>
-                    @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@0,600;0,800;1,300&display=swap');
                     body {
                         font-family: 'Nunito Sans', sans-serif;
                         font-optical-sizing: auto;
